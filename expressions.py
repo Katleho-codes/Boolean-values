01d4b22e5083bb2817b3672014492bd6708aed4f
Katleho-codes <67188889+Katleho-codes@users.noreply.github.com>
2021-06-15 09:57:58 +0200

    Add files via upload

---
+++ expressions.py
@@ -0,0 +1,32 @@
+# What do these expressions evaluate too ? Write a program that prints the results to the terminal
+42 == 42
+if 42 == 42:
+    print(True)
+else:
+        print(False)
+
+3 != 3
+if 3 != 3:
+    print(True)
+else:
+    print(False)
+
+
+3 > 4
+if 3 > 4:
+    print(True)
+else:
+    print(False)
+
+0 < 6
+if 0 < 6:
+    print(True)
+else:
+    print(False)
+
+
+6 > 0
+if 6 > 0:
+    print(True)
+else:
+    print(False)
